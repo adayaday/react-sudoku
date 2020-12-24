3f1bbecaee556d62a581c2669297b8f991da03e5
Xiaojie Hao <hxj.hao@gmail.com>
2020-12-23 16:52:43 -0800

    load easy game when start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import React, { useEffect } from "react";
 import TopBar from "./components/TopBar/TopBar";
 import MainControl from "./containers/MainControl/MainControl";
 import Game from "./containers/Game/Game";
+import { useDispatch } from "react-redux";
+import * as actions from "./store/actions";
 
 function App() {
+  const dispatch = useDispatch();
+  const onNewGameStart = () => dispatch(actions.initNewGameLoading());
+
+  useEffect(() => onNewGameStart(), []);
+
   return (
     <div className="App">
       <TopBar />
